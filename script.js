2bfdb901ac3c84123ed081a22e2b88feb1e59184
Sec-tester116 <sectester116@gmail.com>
2026-01-21 13:41:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const SUPABASE_URL = "https://aoxhkobnpvjnqrbsjhcl.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFveGhrb2JucHZqbnFyYnNqaGNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5ODg2MTksImV4cCI6MjA4NDU2NDYxOX0.H6fIcKV5X4Vce66Xiz5HUKI49JNlF93BYQbNiFMWFx0";
+
+const supabase = supabaseJs.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
+
 let chosen = "";
 
 function choose(who) {
@@ -28,4 +36,5 @@ function sendMessage() {
 
   document.getElementById("message").value = "";
   goBack();
+
 }
